9137d96103d0435ed4a98b166896f386c78186c3
iyr <nighthawk@DESKTOP-DNEAV0F.localdomain>
2018-11-07 23:18:16 -0700

    added mainControl property

--- heavenLi_python/lampClass.py
+++ heavenLi_python/lampClass.py
@@ -8,21 +8,56 @@ class Lamp:
             name='demo',
             angularOffset=0,
             ):
+
+        # Number of Bulbs
         self.numBulbs = numBulbs
+
+        # 0 - Lamp is Circularly Arranged
+        # 1 - Lamp is LinearLy Arranged
         self.arn = arn
+
+        # Name or ID of Lamp
         self.name = name
+
+        # 0 - Bulbs turn to White Light when clock is touched
+        # 1 - Bulbs turn to prev color(s) when clock is touched, default is white when no prev
+        # 2 - Bulbs unaffected by Central Clock input
+        self.mainsControlMode = 0
+
+        # Angle In which bulbs are arranged
         self.angularOffset = angularOffset
+
+
         self.bulbsCurrentHSV = []
         self.bulbsTargetHSV = []
         for i in range(numBulbs):
             self.bulbsCurrentHSV.append((i*(360/numBulbs)+60, 255, 255))
         self.bulbsTargetHSV = self.bulbsCurrentHSV
 
+    def getAngle(self):
+        return self.angularOffset
+
+    def setAngle(self, ang):
+        if ang >= 360:
+            ang -= 360
+        if ang <= 0:
+            ang += 360
+        self.angularOffset = ang
+
+    def getArn(self):
+        return self.arn
+
+    def setArn(self, n):
+        self.arn = n
+
     def getBulbRGB(self, n):
         return colorsys.hsv_to_rgb(
                 self.bulbsCurrentHSV[n][0]/255,
                 self.bulbsCurrentHSV[n][1]/255,
                 self.bulbsCurrentHSV[n][2]/255)
+
+    def getControlMode(self, n):
+        return self.mainsControlMode
 
     def getBulbsRGB(self):
         tmp = []
@@ -44,14 +79,4 @@ class Lamp:
 
     def getNumBulbs(self):
         return self.numBulbs
-
-    def setAngle(self, ang):
-        if ang >= 360:
-            ang -= 360
-        if ang <= 0:
-            ang += 360
-        self.angularOffset = ang
-
-    def getAngle(self):
-        return self.angularOffset
 
