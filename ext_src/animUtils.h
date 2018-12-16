c867bede66975274354262ea3d12d73d05bc5a09
iyr <forwardsweep@gmail.com>
2018-12-16 14:25:07 -0700

    quack

--- ext_src/animUtils.h
+++
@@ -1,4 +0,0 @@
-#include <Python.h>
-#pragma once
-static PyObject *animCurveBounce(PyObject *self, PyObject *args);
-static PyObject *animCurve(PyObject *self, PyObject *args);
--- ext_src/pysrc/animUtils.py
+++
@@ -1,9 +0,0 @@
-def animCurve(c):
-    return -2.25*pow(float(c)/(1.5), 2) + 1.0
-
-def animCurveBounce(c):
-    if (c >= 1.0):
-        return 0
-    else:
-        return -3.0*pow((float(c)-(0.14167))/(1.5), 2)+1.02675926
-
