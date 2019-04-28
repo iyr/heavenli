4daac2947dc6e17b9df1a7db4b03aa2a3fe3d79c
iyr <forwardsweep@gmail.com>
2019-04-27 18:23:59 -0600

    draw code now uses vertex buffer object for data storage

--- heavenLi_pyopengl/infoUtils/printText.cpp
+++ heavenLi_pyopengl/infoUtils/printText.cpp
@@ -12,23 +12,27 @@
 #include <string>
 using namespace std;
 
-GLfloat*    stringVertexBuffer   =  NULL;
-GLfloat*    stringColourBuffer   =  NULL;
-GLuint*     stringIndices        =  NULL;
+GLfloat     *stringCoordBuffer  = NULL;  // Stores (X, Y) (float) for each vertex
+GLfloat     *stringColorBuffer  = NULL;  // Stores (R, G, B) (float) for each vertex
+GLushort    *stringIndices      = NULL;  // Stores index corresponding to each vertex
 GLuint      stringVerts;
-std::string      prevString;
+std::string prevString;
+Matrix      stringMVP;                   // Transformation matrix passed to shader
+Params      stringPrevState;             // Stores transformations to avoid redundant recalculation
+GLuint      stringVBO;                   // Vertex Buffer Object ID
+GLboolean   stringFirstRun = GL_TRUE;    // Determines if function is running for the first time (for VBO initialization)
 
 PyObject* printText_drawButtons(PyObject* self, PyObject *args) {
    PyObject *colourPyTup;
    PyObject *Pystring;
 
-   float posX, posY, scale, w2h;
-   float textColor[3];
+   GLfloat gx, gy, scale, w2h, ao=0.0f;
+   GLfloat textColor[3];
 
    // Parse Inputs
    if ( !PyArg_ParseTuple(args,
             "ffffOO",
-            &posX, &posY,
+            &gx, &gy,
             &scale,
             &w2h,
             &Pystring,
@@ -45,10 +49,10 @@ PyObject* printText_drawButtons(PyObject* self, PyObject *args) {
    textColor[2]   = float(PyFloat_AsDouble(PyTuple_GetItem(colourPyTup, 2)));
 
    // (Re)allocate and Define Geometry/Color buffers
-   if (  stringVertexBuffer   == NULL  ||
-         stringColourBuffer   == NULL  ||
-         stringIndices        == NULL  ||
-         prevString           != inputString){
+   if (  stringCoordBuffer == NULL        ||
+         stringColorBuffer == NULL        ||
+         stringIndices     == NULL        ||
+         prevString        != inputString ){
 
       //printf("Input String: %s\n", inputString.c_str());
 
@@ -81,53 +85,180 @@ PyObject* printText_drawButtons(PyObject* self, PyObject *args) {
       }
 
       stringVerts = verts.size()/2;
-      //printf("stringVerts: %i\n", stringVerts);
-      //printf("lineWidth: %f\n", lineWidth);
 
       // Pack Vertics and Colors into global array buffers
-      if (stringVertexBuffer == NULL) {
-         stringVertexBuffer = new GLfloat[stringVerts*2];
+      if (stringCoordBuffer == NULL) {
+         stringCoordBuffer = new GLfloat[stringVerts*2];
       } else {
-         delete [] stringVertexBuffer;
-         stringVertexBuffer = new GLfloat[stringVerts*2];
+         delete [] stringCoordBuffer;
+         stringCoordBuffer = new GLfloat[stringVerts*2];
       }
 
-      if (stringColourBuffer == NULL) {
-         stringColourBuffer = new GLfloat[stringVerts*3];
+      if (stringColorBuffer == NULL) {
+         stringColorBuffer = new GLfloat[stringVerts*3];
       } else {
-         delete [] stringColourBuffer;
-         stringColourBuffer = new GLfloat[stringVerts*3];
+         delete [] stringColorBuffer;
+         stringColorBuffer = new GLfloat[stringVerts*3];
       }
 
       if (stringIndices == NULL) {
-         stringIndices = new GLuint[stringVerts];
+         stringIndices = new GLushort[stringVerts];
       } else {
          delete [] stringIndices;
-         stringIndices = new GLuint[stringVerts];
+         stringIndices = new GLushort[stringVerts];
       }
 
       for (unsigned int i = 0; i < stringVerts; i++) {
-         stringVertexBuffer[i*2+0]  = verts[i*2+0];
-         stringVertexBuffer[i*2+1]  = verts[i*2+1];
-         stringIndices[i]           = i;
-         stringColourBuffer[i*3+0]  = colrs[i*3+0];
-         stringColourBuffer[i*3+1]  = colrs[i*3+1];
-         stringColourBuffer[i*3+2]  = colrs[i*3+2];
+         stringCoordBuffer[i*2+0]  = verts[i*2+0];
+         stringCoordBuffer[i*2+1]  = verts[i*2+1];
+         stringIndices[i]          = i;
+         stringColorBuffer[i*3+0]  = colrs[i*3+0];
+         stringColorBuffer[i*3+1]  = colrs[i*3+1];
+         stringColorBuffer[i*3+2]  = colrs[i*3+2];
       }
 
       prevString = inputString;
-   }
-   glPushMatrix();
-   glTranslatef(posX*w2h, posY, 0.0f);
-   if (w2h <= 1.0) {
-         scale = scale*w2h;
+      
+      // Calculate Initial Transformation Matrix
+      Matrix Ortho;
+      Matrix ModelView;
+
+      float left = -1.0f*w2h, right = 1.0f*w2h, bottom = 1.0f, top = 1.0f, near = 1.0f, far = 1.0f;
+      MatrixLoadIdentity( &Ortho );
+      MatrixLoadIdentity( &ModelView );
+      MatrixOrtho( &Ortho, left, right, bottom, top, near, far );
+      MatrixTranslate( &ModelView, 1.0f*gx, 1.0f*gy, 0.0f );
+      if (w2h <= 1.0f) {
+         MatrixScale( &ModelView, scale, scale*w2h, 1.0f );
+      } else {
+         MatrixScale( &ModelView, scale/w2h, scale, 1.0f );
+      }
+      MatrixRotate( &ModelView, -ao, 0.0f, 0.0f, 1.0f);
+      MatrixMultiply( &stringMVP, &ModelView, &Ortho );
+
+      stringPrevState.ao = ao;
+      stringPrevState.dx = gx;
+      stringPrevState.dy = gy;
+      stringPrevState.sx = scale;
+      stringPrevState.sy = scale;
+      stringPrevState.w2h = w2h;
+
+      // Create buffer object if one does not exist, otherwise, delete and make a new one
+      if (stringFirstRun == GL_TRUE) {
+         stringFirstRun = GL_FALSE;
+         glGenBuffers(1, &stringVBO);
+      } else {
+         glDeleteBuffers(1, &stringVBO);
+         glGenBuffers(1, &stringVBO);
+      }
+
+      // Set active VBO
+      glBindBuffer(GL_ARRAY_BUFFER, stringVBO);
+
+      // Allocate space to hold all vertex coordinate and color data
+      glBufferData(GL_ARRAY_BUFFER, 5*sizeof(GLfloat)*stringVerts, NULL, GL_STATIC_DRAW);
+
+      // Convenience variables
+      GLuint64 offset = 0;
+      GLuint vertAttribCoord = glGetAttribLocation(3, "vertCoord");
+      GLuint vertAttribColor = glGetAttribLocation(3, "vertColor");
+
+      // Load Vertex coordinate data into VBO
+      glBufferSubData(GL_ARRAY_BUFFER, offset, sizeof(GLfloat)*2*stringVerts, stringCoordBuffer);
+      // Define how the Vertex coordinate data is layed out in the buffer
+      glVertexAttribPointer(vertAttribCoord, 2, GL_FLOAT, GL_FALSE, 2*sizeof(GLfloat), (GLuint64*)offset);
+      // Enable the vertex attribute
+      glEnableVertexAttribArray(vertAttribCoord);
+
+      // Update offset to begin storing data in latter part of the buffer
+      offset += 2*sizeof(GLfloat)*stringVerts;
+
+      // Load Vertex coordinate data into VBO
+      glBufferSubData(GL_ARRAY_BUFFER, offset, sizeof(GLfloat)*3*stringVerts, stringColorBuffer);
+      // Define how the Vertex color data is layed out in the buffer
+      glVertexAttribPointer(vertAttribColor, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLuint64*)offset);
+      // Enable the vertex attribute
+      glEnableVertexAttribArray(vertAttribColor);
    }
 
-   glScalef(scale, scale, 1);
-   glColorPointer(3, GL_FLOAT, 0, stringColourBuffer);
-   glVertexPointer(2, GL_FLOAT, 0, stringVertexBuffer);
-   glDrawElements( GL_TRIANGLES, stringVerts, GL_UNSIGNED_INT, stringIndices);
-   glPopMatrix();
+   // Geometry already calculated, update colors
+   /*
+    * Iterate through each color channel 
+    * 0 - RED
+    * 1 - GREEN
+    * 2 - BLUE
+    */
+   for (GLuint i = 0; i < 3; i++) {
+      if (float(textColor[i]) != stringColorBuffer[i] ){
+         for (GLuint k = 0; k < stringVerts; k++) {
+            if (float(textColor[i]) != stringColorBuffer[i + k*3]){
+               stringColorBuffer[k*3 + i] = float(textColor[i]);
+            }
+         }
+         // Update Contents of VBO
+         // Set active VBO
+         glBindBuffer(GL_ARRAY_BUFFER, stringVBO);
+         // Convenience variable
+         GLuint64 offset = 2*sizeof(GLfloat)*stringVerts;
+         // Load Vertex Color data into VBO
+         glBufferSubData(GL_ARRAY_BUFFER, offset, sizeof(GLfloat)*3*stringVerts, stringColorBuffer);
+      }
+   }
+
+   // Update Transfomation Matrix if any change in parameters
+   if (  stringPrevState.ao != ao     ||
+         stringPrevState.dx != gx     ||
+         stringPrevState.dy != gy     ||
+         stringPrevState.sx != scale  ||
+         stringPrevState.sy != scale  ||
+         stringPrevState.w2h != w2h   ){
+      
+      Matrix Ortho;
+      Matrix ModelView;
+
+      float left = -1.0f*w2h, right = 1.0f*w2h, bottom = 1.0f, top = 1.0f, near = 1.0f, far = 1.0f;
+      MatrixLoadIdentity( &Ortho );
+      MatrixLoadIdentity( &ModelView );
+      MatrixOrtho( &Ortho, left, right, bottom, top, near, far );
+      MatrixTranslate( &ModelView, 1.0f*gx, 1.0f*gy, 0.0f );
+      if (w2h <= 1.0f) {
+         MatrixScale( &ModelView, scale, scale*w2h, 1.0f );
+      } else {
+         MatrixScale( &ModelView, scale/w2h, scale, 1.0f );
+      }
+      MatrixRotate( &ModelView, -ao, 0.0f, 0.0f, 1.0f);
+      MatrixMultiply( &stringMVP, &ModelView, &Ortho );
+
+      stringPrevState.ao = ao;
+      stringPrevState.dx = gx;
+      stringPrevState.dy = gy;
+      stringPrevState.sx = scale;
+      stringPrevState.sy = scale;
+      stringPrevState.w2h = w2h;
+
+      // Set active VBO
+      glBindBuffer(GL_ARRAY_BUFFER, stringVBO);
+      // Define how the Vertex color data is layed out in the buffer
+      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)(2*sizeof(GLfloat)*stringVerts));
+   }
+
+   // Pass Transformation Matrix to shader
+   glUniformMatrix4fv( 0, 1, GL_FALSE, &stringMVP.mat[0][0] );
+
+   // Set active VBO
+   glBindBuffer(GL_ARRAY_BUFFER, stringVBO);
+
+   // Define how the Vertex coordinate data is layed out in the buffer
+   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(GLfloat), 0);
+   // Define how the Vertex color data is layed out in the buffer
+   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)(2*sizeof(GLfloat)*stringVerts));
+   //glEnableVertexAttribArray(0);
+   //glEnableVertexAttribArray(1);
+   glDrawArrays(GL_TRIANGLES, 0, stringVerts);
+
+   // Unbind Buffer Object
+   glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 
    Py_RETURN_NONE;
 }
