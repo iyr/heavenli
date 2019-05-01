b27da6e5b0ed3c138daf937939465da51ba94a00
iyr <forwardsweep@gmail.com>
2019-04-30 19:34:34 -0600

    draw code now uses vertex buffer object for data storage

--- heavenLi_pyopengl/buttonUtils/drawGranRocker.cpp
+++ heavenLi_pyopengl/buttonUtils/drawGranRocker.cpp
@@ -12,28 +12,31 @@
 
 using namespace std;
 
-GLfloat*    granRockerVertexBuffer  = NULL;
-GLfloat*    granRockerColorBuffer   = NULL;
-GLushort*   granRockerIndices       = NULL;
+GLfloat     *granRockerCoordBuffer  = NULL;
+GLfloat     *granRockerColorBuffer   = NULL;
+GLushort    *granRockerIndices       = NULL;
 GLuint      granRockerVerts;
+Matrix      granRockerMVP;                   // Transformation matrix passed to shader
+Params      granRockerPrevState;             // Stores transformations to avoid redundant recalculation
+GLuint      granRockerVBO;                   // Vertex Buffer Object ID
+GLboolean   granRockerFirstRun = GL_TRUE;    // Determines if function is running for the first time (for VBO initialization)
 
 PyObject* drawGranRocker_drawButtons(PyObject *self, PyObject *args) {
    PyObject *py_faceColor;
    PyObject *py_detailColor;
-   //PyObject *py_tuple;
    int   numHues;
-   float posX, posY, rotation, w2h, scale, tDiff;
-   float faceColor[3];
-   float detailColor[3];
-   float black[3] = {0.0, 0.0, 0.0};
-   float white[3] = {1.0, 1.0, 1.0};
+   GLfloat gx, gy, rotation, w2h, scale, tDiff, ao=0.0f;
+   GLfloat faceColor[3];
+   GLfloat detailColor[3];
+   GLfloat black[3] = {0.0, 0.0, 0.0};
+   GLfloat white[3] = {1.0, 1.0, 1.0};
    char circleSegments = 45;
 
    // Parse Inputs
    if (!PyArg_ParseTuple(args,
             "ffOOlffff",
-            &posX,
-            &posY,
+            &gx,
+            &gy,
             &py_faceColor,
             &py_detailColor,
             &numHues,
@@ -53,7 +56,7 @@ PyObject* drawGranRocker_drawButtons(PyObject *self, PyObject *args) {
    detailColor[2] = float(PyFloat_AsDouble(PyTuple_GetItem(py_detailColor, 2)));
 
    // Allocate and Define Geometry/Color buffers
-   if (  granRockerVertexBuffer  == NULL  ||
+   if (  granRockerCoordBuffer   == NULL  ||
          granRockerColorBuffer   == NULL  ||
          granRockerIndices       == NULL  ){
       printf("Initializing Geometry for Granularity Rocker\n");
@@ -150,11 +153,11 @@ PyObject* drawGranRocker_drawButtons(PyObject *self, PyObject *args) {
       granRockerVerts = verts.size()/2;
 
       // Pack Vertics and Colors into global array buffers
-      if (granRockerVertexBuffer == NULL) {
-         granRockerVertexBuffer = new GLfloat[granRockerVerts*2];
+      if (granRockerCoordBuffer == NULL) {
+         granRockerCoordBuffer = new GLfloat[granRockerVerts*2];
       } else {
-         delete [] granRockerVertexBuffer;
-         granRockerVertexBuffer = new GLfloat[granRockerVerts*2];
+         delete [] granRockerCoordBuffer;
+         granRockerCoordBuffer = new GLfloat[granRockerVerts*2];
       }
 
       if (granRockerColorBuffer == NULL) {
@@ -172,28 +175,129 @@ PyObject* drawGranRocker_drawButtons(PyObject *self, PyObject *args) {
       }
 
       for (unsigned int i = 0; i < granRockerVerts; i++) {
-         granRockerVertexBuffer[i*2]   = verts[i*2];
-         granRockerVertexBuffer[i*2+1] = verts[i*2+1];
+         granRockerCoordBuffer[i*2]    = verts[i*2];
+         granRockerCoordBuffer[i*2+1]  = verts[i*2+1];
          granRockerIndices[i]          = i;
          granRockerColorBuffer[i*3+0]  = colrs[i*3+0];
          granRockerColorBuffer[i*3+1]  = colrs[i*3+1];
          granRockerColorBuffer[i*3+2]  = colrs[i*3+2];
       }
+
+      // Calculate Initial Transformation Matrix
+      Matrix Ortho;
+      Matrix ModelView;
+
+      float left = -1.0f*w2h, right = 1.0f*w2h, bottom = 1.0f, top = 1.0f, near = 1.0f, far = 1.0f;
+      MatrixLoadIdentity( &Ortho );
+      MatrixLoadIdentity( &ModelView );
+      MatrixOrtho( &Ortho, left, right, bottom, top, near, far );
+      MatrixTranslate( &ModelView, 1.0f*gx, 1.0f*gy, 0.0f );
+      if (w2h <= 1.0f) {
+         MatrixScale( &ModelView, scale, scale*w2h, 1.0f );
+      } else {
+         MatrixScale( &ModelView, scale/w2h, scale, 1.0f );
+      }
+      MatrixRotate( &ModelView, -ao, 0.0f, 0.0f, 1.0f);
+      MatrixMultiply( &granRockerMVP, &ModelView, &Ortho );
+
+      granRockerPrevState.ao = ao;
+      granRockerPrevState.dx = gx;
+      granRockerPrevState.dy = gy;
+      granRockerPrevState.sx = scale;
+      granRockerPrevState.sy = scale;
+      granRockerPrevState.w2h = w2h;
+
+      // Create buffer object if one does not exist, otherwise, delete and make a new one
+      if (granRockerFirstRun == GL_TRUE) {
+         granRockerFirstRun = GL_FALSE;
+         glGenBuffers(1, &granRockerVBO);
+      } else {
+         glDeleteBuffers(1, &granRockerVBO);
+         glGenBuffers(1, &granRockerVBO);
+      }
+
+      // Set active VBO
+      glBindBuffer(GL_ARRAY_BUFFER, granRockerVBO);
+
+      // Allocate space to hold all vertex coordinate and color data
+      glBufferData(GL_ARRAY_BUFFER, 5*sizeof(GLfloat)*granRockerVerts, NULL, GL_STATIC_DRAW);
+
+      // Convenience variables
+      GLintptr offset = 0;
+      GLuint vertAttribCoord = glGetAttribLocation(3, "vertCoord");
+      GLuint vertAttribColor = glGetAttribLocation(3, "vertColor");
+
+      // Load Vertex coordinate data into VBO
+      glBufferSubData(GL_ARRAY_BUFFER, offset, sizeof(GLfloat)*2*granRockerVerts, granRockerCoordBuffer);
+      // Define how the Vertex coordinate data is layed out in the buffer
+      glVertexAttribPointer(vertAttribCoord, 2, GL_FLOAT, GL_FALSE, 2*sizeof(GLfloat), (GLintptr*)offset);
+      // Enable the vertex attribute
+      glEnableVertexAttribArray(vertAttribCoord);
+
+      // Update offset to begin storing data in latter part of the buffer
+      offset += 2*sizeof(GLfloat)*granRockerVerts;
+
+      // Load Vertex coordinate data into VBO
+      glBufferSubData(GL_ARRAY_BUFFER, offset, sizeof(GLfloat)*3*granRockerVerts, granRockerColorBuffer);
+      // Define how the Vertex color data is layed out in the buffer
+      glVertexAttribPointer(vertAttribColor, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLintptr*)offset);
+      // Enable the vertex attribute
+      glEnableVertexAttribArray(vertAttribColor);
    }
 
-   glPushMatrix();
-   if (w2h >= 1) {
-      glTranslatef(posX*w2h, posY, 0);
-      glScalef(scale, scale, 1);
-   } else {
-      glTranslatef(posX*w2h, posY*w2h, 0);
-      glScalef(scale*w2h, scale*w2h, 1);
+   // Update Transfomation Matrix if any change in parameters
+   if (  granRockerPrevState.ao != ao     ||
+         granRockerPrevState.dx != gx     ||
+         granRockerPrevState.dy != gy     ||
+         granRockerPrevState.sx != scale  ||
+         granRockerPrevState.sy != scale  ||
+         granRockerPrevState.w2h != w2h   ){
+      
+      Matrix Ortho;
+      Matrix ModelView;
+
+      float left = -1.0f*w2h, right = 1.0f*w2h, bottom = 1.0f, top = 1.0f, near = 1.0f, far = 1.0f;
+      MatrixLoadIdentity( &Ortho );
+      MatrixLoadIdentity( &ModelView );
+      MatrixOrtho( &Ortho, left, right, bottom, top, near, far );
+      MatrixTranslate( &ModelView, 1.0f*gx, 1.0f*gy, 0.0f );
+      if (w2h <= 1.0f) {
+         MatrixScale( &ModelView, scale, scale*w2h, 1.0f );
+      } else {
+         MatrixScale( &ModelView, scale/w2h, scale, 1.0f );
+      }
+      MatrixRotate( &ModelView, -ao, 0.0f, 0.0f, 1.0f);
+      MatrixMultiply( &granRockerMVP, &ModelView, &Ortho );
+
+      granRockerPrevState.ao = ao;
+      granRockerPrevState.dx = gx;
+      granRockerPrevState.dy = gy;
+      granRockerPrevState.sx = scale;
+      granRockerPrevState.sy = scale;
+      granRockerPrevState.w2h = w2h;
+
+      // Set active VBO
+      glBindBuffer(GL_ARRAY_BUFFER, granRockerVBO);
+      // Define how the Vertex color data is layed out in the buffer
+      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)(2*sizeof(GLfloat)*granRockerVerts));
    }
-   glRotatef(rotation, 0, 0, 1);
-   glColorPointer(3, GL_FLOAT, 0, granRockerColorBuffer);
-   glVertexPointer(2, GL_FLOAT, 0, granRockerVertexBuffer);
-   glDrawElements( GL_TRIANGLES, granRockerVerts, GL_UNSIGNED_SHORT, granRockerIndices);
-   glPopMatrix();
-   
+
+   // Pass Transformation Matrix to shader
+   glUniformMatrix4fv( 0, 1, GL_FALSE, &granRockerMVP.mat[0][0] );
+
+   // Set active VBO
+   glBindBuffer(GL_ARRAY_BUFFER, granRockerVBO);
+
+   // Define how the Vertex coordinate data is layed out in the buffer
+   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(GLfloat), 0);
+   // Define how the Vertex color data is layed out in the buffer
+   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)(2*sizeof(GLfloat)*granRockerVerts));
+   //glEnableVertexAttribArray(0);
+   //glEnableVertexAttribArray(1);
+   glDrawArrays(GL_TRIANGLES, 0, granRockerVerts);
+
+   // Unbind Buffer Object
+   glBindBuffer(GL_ARRAY_BUFFER, 0);
+
    Py_RETURN_NONE;
 }
