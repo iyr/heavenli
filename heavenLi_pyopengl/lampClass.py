40e4d204ae376775b8a02f7e0a1a03941900e8fb
iyr <forwardsweep@gmail.com>
2019-06-25 21:58:35 -0600

    refactoring, plugin tweaks

--- heavenLi_pyopengl/lampClass.py
+++ heavenLi_pyopengl/lampClass.py
@@ -3,43 +3,48 @@ from rangeUtils import *
 
 class Lamp:
     
-    def __init__(self,
-            numBulbs=3,
-            arn=0,
-            name='demo',
-            angularOffset=0,
-            mainsControlMode=-1,
-            ):
+    def __init__(self):#,
+            #numBulbs=3,
+            #arrangement=0,
+            #alias='demo',
+            #angularOffset=0,
+            #masterSwitchBehavior=-1,
+            #):
 
         # Angle In which bulbs are arranged
-        self.angularOffset = angularOffset
+        self.angularOffset = None
 
         # 0 - Lamp is Circularly Arranged
         # 1 - Lamp is LinearLy Arranged
-        self.arn = arn
-
-        self.frameCursors = [0, 0, 0, 0, 0, 0]
+        self.arrangement = None
 
         # -1 - Bulbs turn to White Light when clock is touched
         # -2 - Bulbs turn to prev color(s) when clock is touched, default is white when no prev
         # -3 - Bulbs unaffected by Central Clock input
         # 0+ - Bulbs turn to selected (by index) user-saved color profile
-        self.mainsControlMode = -1
+        self.masterSwitchBehavior = -1
 
         self.mainLightOn = False
 
-        # Name or ID of Lamp
-        self.name = name
+        # Name or ID
+        self.alias = None
 
         # Number of Bulbs
-        self.numBulbs = numBulbs
+        self.numBulbs = None
 
         self.bulbsPreviousHSV = []
         self.bulbsCurrentHSV = []
         self.bulbsTargetHSV = []
-        for i in range(self.numBulbs):
-            self.bulbsCurrentHSV.append((i*(1.0/self.numBulbs)+0.16667, 1.00, 1.00))
+        for i in range(10):
+            self.bulbsCurrentHSV.append((i*(1.0/3)+0.16667, 1.00, 1.00))
         self.bulbsTargetHSV = self.bulbsCurrentHSV.copy()
+
+    def alias(self):
+        return self.alias
+
+    def setAlias(self, newAlias):
+        self.alias = newAlias
+        return
 
     # Get/Set angle offset for lamp
     def getAngle(self):
@@ -51,13 +56,15 @@ class Lamp:
         if ang <= 0:
             ang += 360
         self.angularOffset = ang
+        return
 
     # Set/Get arrangement for lamp
     def getArn(self):
-        return self.arn
+        return self.arrangement
 
     def setArn(self, n):
-        self.arn = n
+        self.arrangement = n
+        return
 
     # Get list of current RGB values for each bulb
     def getBulbRGB(self, n):
@@ -67,8 +74,8 @@ class Lamp:
                 self.bulbsCurrentHSV[n][2])
         return tmp
 
-    # Get list of targe RGB values for each bulb
-    def getBulbtRGB(self, n):
+    # Get list of target RGB values for each bulb
+    def getBulbTargetRGB(self, n):
         tmp = colorsys.hsv_to_rgb(
                 self.bulbsTargetHSV[n][0],
                 self.bulbsTargetHSV[n][1],
@@ -83,10 +90,10 @@ class Lamp:
     def getBulbtHSV(self, bulb):
         return self.bulbsTargetHSV[bulb]
 
-    def setBulbtHSV(self, bulb, HSVcolor):
+    def setBulbTargetHSV(self, bulb, HSVcolor):
         self.bulbsTargetHSV[bulb] = HSVcolor
 
-    def setBulbstHSV(self, HSVcolor):
+    def setBulbsTargetHSV(self, HSVcolor):
         for i in range(self.numBulbs):
             self.bulbsTargetHSV[i] = HSVcolor
 
@@ -111,80 +118,83 @@ class Lamp:
 
     # If current colors =/= target colors, animate smooth color transition
     def updateBulbs(self, frametime):
+        
+        if (self.numBulbs is None or self.numBulbs < 1):
+            pass
+        else:
+            for i in range(0, self.numBulbs):
+                colC = colorsys.hsv_to_rgb(
+                        self.bulbsCurrentHSV[i][0],
+                        self.bulbsCurrentHSV[i][1],
+                        self.bulbsCurrentHSV[i][2])
+                colT = colorsys.hsv_to_rgb(
+                        self.bulbsTargetHSV[i][0],
+                        self.bulbsTargetHSV[i][1],
+                        self.bulbsTargetHSV[i][2])
 
-        for i in range(0, self.numBulbs):
-            colC = colorsys.hsv_to_rgb(
-                    self.bulbsCurrentHSV[i][0],
-                    self.bulbsCurrentHSV[i][1],
-                    self.bulbsCurrentHSV[i][2])
-            colT = colorsys.hsv_to_rgb(
-                    self.bulbsTargetHSV[i][0],
-                    self.bulbsTargetHSV[i][1],
-                    self.bulbsTargetHSV[i][2])
+                if (colC != colT):
+                    curR = colC[0]
+                    curG = colC[1]
+                    curB = colC[2]
+                    tarR = colT[0]
+                    tarG = colT[1]
+                    tarB = colT[2]
+                    difR = abs(curR - tarR)
+                    difG = abs(curG - tarG)
+                    difB = abs(curB - tarB)
+                    rd = 0
+                    gd = 0
+                    bd = 0
+                    threshold = 0.05
+                    #delta = frametime + i/self.numBulbs
+                    #delta /= 2
+                    delta = frametime
+                    delta *= ((i+2)*2)/(self.numBulbs*3)
+    
+                    if (difR > threshold):
+                        if (tarR > curR):
+                            rd = delta
+                        else:
+                            rd = -(delta)
+                    if (difG > threshold):
+                        if (tarG > curG):
+                            gd = delta
+                        else:
+                            gd = -(delta)
+                    if (difB > threshold):
+                        if (tarB > curB):
+                            bd = delta
+                        else:
+                            bd = -(delta)
 
-            if (colC != colT):
-                curR = colC[0]
-                curG = colC[1]
-                curB = colC[2]
-                tarR = colT[0]
-                tarG = colT[1]
-                tarB = colT[2]
-                difR = abs(curR - tarR)
-                difG = abs(curG - tarG)
-                difB = abs(curB - tarB)
-                rd = 0
-                gd = 0
-                bd = 0
-                threshold = 0.05
-                #delta = frametime + i/self.numBulbs
-                #delta /= 2
-                delta = frametime
-                delta *= ((i+2)*2)/(self.numBulbs*3)
-
-                if (difR > threshold):
-                    if (tarR > curR):
-                        rd = delta
+                    if (difR > threshold):
+                        difR = curR + rd
                     else:
-                        rd = -(delta)
-                if (difG > threshold):
-                    if (tarG > curG):
-                        gd = delta
+                        difR = tarR
+
+                    if (difG > threshold):
+                        difG = curG + gd
                     else:
-                        gd = -(delta)
-                if (difB > threshold):
-                    if (tarB > curB):
-                        bd = delta
+                        difG = tarG
+
+                    if (difB > threshold):
+                        difB = curB + bd
                     else:
-                        bd = -(delta)
+                        difB = tarB
 
-                if (difR > threshold):
-                    difR = curR + rd
-                else:
-                    difR = tarR
-
-                if (difG > threshold):
-                    difG = curG + gd
-                else:
-                    difG = tarG
-
-                if (difB > threshold):
-                    difB = curB + bd
-                else:
-                    difB = tarB
-
-                difR = constrain(difR, 0.0, 1.0)
-                difG = constrain(difG, 0.0, 1.0)
-                difB = constrain(difB, 0.0, 1.0)
-                self.bulbsCurrentHSV[i] = colorsys.rgb_to_hsv(difR, difG, difB)
-            #self.bulbsCurrentHSV[i] = (
-                    #min(self.bulbsTargetHSV[i][0]+frametime, 1.0),
-                    #min(self.bulbsTargetHSV[i][1]+frametime, 1.0),
-                    #min(self.bulbsTargetHSV[i][2]+frametime, 1.0)
-                    #)
+                    difR = constrain(difR, 0.0, 1.0)
+                    difG = constrain(difG, 0.0, 1.0)
+                    difB = constrain(difB, 0.0, 1.0)
+                    self.bulbsCurrentHSV[i] = colorsys.rgb_to_hsv(difR, difG, difB)
+                #self.bulbsCurrentHSV[i] = (
+                        #min(self.bulbsTargetHSV[i][0]+frametime, 1.0),
+                        #min(self.bulbsTargetHSV[i][1]+frametime, 1.0),
+                        #min(self.bulbsTargetHSV[i][2]+frametime, 1.0)
+                        #)
 
     # Returns the control mode for the lamp
     def getControlMode(self, n):
-        return self.mainsControlMode
+        return self.masterSwitchBehavior
 
     # Used for animating Bulb Color Transitions
     # frameCursors is and array of floats where the cursor
@@ -202,15 +212,21 @@ class Lamp:
 
         # Lamp is being turned on
         if (self.mainLightOn == False) and (lightOn == True):
-            if self.mainsControlMode == -1:
-                for i in range(self.numBulbs):
-                    self.bulbsTargetHSV[i] = (1.0, 0.0, 1.0)
+            if self.masterSwitchBehavior == -1:
+                try:
+                    for i in range(self.numBulbs):
+                        self.bulbsTargetHSV[i] = (1.0, 0.0, 1.0)
+                except Exception as OOF:
+                    print("Error:", OOF)
 
         # Lamp is being turn off
         if (self.mainLightOn == True) and (lightOn == False):
-            if self.mainsControlMode >= -2:
-                for i in range(self.numBulbs):
-                    self.bulbsTargetHSV[i] = (0.0, 0.0, 0.0)
+            if self.masterSwitchBehavior >= -2:
+                try:
+                    for i in range(self.numBulbs):
+                        self.bulbsTargetHSV[i] = (0.0, 0.0, 0.0)
+                except Exception as OOF:
+                    print("Error:", OOF)
         self.mainLightOn = lightOn
 
 
