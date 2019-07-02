a8e403b5b8ea42d3fff2804925fea5d301a305fb
iyr <forwardsweep@gmail.com>
2019-07-01 18:16:47 -0600

    plugin properly parses Client ID bytes

--- heavenLi_pyopengl/plugins/ArduinoUART/plugin.py
+++ heavenLi_pyopengl/plugins/ArduinoUART/plugin.py
@@ -72,7 +72,7 @@ class Plugin():
             self.connectedLamps = []
 
             # ID of the client device
-            self.clientID = None
+            self.clientID = [None, None]
 
         def __del__(self):
             self.serialDevice.close()
@@ -88,18 +88,36 @@ class Plugin():
                     zeroByte = b'\x00'
                     mess = self.serialDevice.read_until( zeroByte )
                     print("Data BEFORE COBS decoding: ", mess)
-                    mess = str(cobs.decode( mess[0:-1] )[:-1])[2:-1]
+                    #mess = str(cobs.decode( mess[0:-1] )[:-1])[2:-1] ORIGINAL
+                    mess = str(cobs.decode( mess[:-1] ) )[2:-1]
                     print("Data received. Packet:", mess)
 
-                    if ('CID:' in str(mess)):
-                        pos = str(mess).index('CID:')+4
-                        demess = mess[pos:pos+8].encode(encoding='utf-8')
-                        print("Received client ID:")
-                        print(demess[2:4], demess[6:8])
-                        print(int(demess[2:4], 16), int(demess[6:8], 16))
-                        print(chr(int(demess[2:4], 16)), chr(int(demess[6:8], 16)))
-                        self.clientID = demess
-                        self.serialDevice.flushInput()
+                    if ("CID:" in str(mess)):
+                        pos = str(mess).index("CID:")+4
+                        if ("\\x" in mess):
+                            print("CID contains improperly formatted bytes")
+                            print("Ungarbling bytes...")
+                            demess = mess[pos:pos+8].encode(encoding="utf-8")
+                            ID_a = int(demess[2:4], 16)
+                            ID_b = int(demess[6:8], 16)
+                            print("ungarbled demess:", ID_a, ID_b, chr(ID_a), chr(ID_b))
+                        else:
+                            demess = mess[pos:pos+2]
+                            ID_a = ord(demess[0])
+                            ID_b = ord(demess[1])
+                            print("demess:", ID_a, ID_b)
+
+                        if (    ID_a == 255 or
+                                ID_b == 255 ):
+                            print("Invalid Client ID:", ID_a, ID_b)
+                            newID = [ord('A'), ord('b')]
+                            #print("Sending:", newID, chr(newID[0]), chr(newID[1]))
+                            self.setClientID(newID)
+                        else:
+                            print("Received client ID:", ID_a, ID_b)
+                            self.clientID[0] = ID_a
+                            self.clientID[1] = ID_b
+                            self.serialDevice.flushInput()
 
             except Exception as OOF:
                 self.synReceived = False
@@ -131,11 +149,13 @@ class Plugin():
                         self.serialDevice.write(enmass)
 
                     # If Ack Packet received, we know this device is a client
-                    elif (mess == "ACK" and self.synReceived == True):
+                    elif (  mess == "ACK" and 
+                            self.synReceived == True and
+                            self.connectionEstablished == False):
+                        print("CONNECTION ESTABLISHED :D")
                         self.isClient = 1
                         self.connectionEstablished = True
                         self.requestClientID()
-                        #print("CONNECTION ESTABLISHED :D")
                     else:
                         pass
                         print("Data received. Packet:", mess)
@@ -149,27 +169,28 @@ class Plugin():
                 print("Error Decoding Packet: ", OOF)
 
         def requestNumBulbs(self, lamp):
-            enmass = cobs.encode(b'NB?R')+b'\x01'+b'\x00'
+            enmass = cobs.encode(b'NB?')+b'\x01'+b'\x00'
             self.serialDevice.write(enmass)
             pass
             return
 
         def setNumBulbs(self, lamp, newNumBulbs):
             varInBytes = (newNumBulbs).to_bytes(1, byteorder='little')
-            enmass = cobs.encode(b'NB!W')+varInBytes+b'\x01'+b'\x00'
+            enmass = cobs.encode(b'NB:')+varInBytes+b'\x01'+b'\x00'
             self.serialDevice.write(enmass)
             pass
             return
 
         def requestClientID(self):
-            enmass = cobs.encode(b'CID?R')+b'\x01'+b'\x00'
+            enmass = cobs.encode(b'CID?')+b'\x01'+b'\x00'
             self.serialDevice.write(enmass)
             #print("Requesting Client ID on port:", self.port)
             pass
             return
 
-        def writeClientID(self):
-            enmass = cobs.encode(b'ID!W')+b'\x01'+b'\x00'
+        def setClientID(self, newID):
+            enmass = cobs.encode(b'CID:'+bytes(newID))+b'\x01'+b'\x00'
+            print(enmass)
             self.serialDevice.write(enmass)
             pass
             return
