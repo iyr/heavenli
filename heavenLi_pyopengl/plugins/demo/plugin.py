c64567bdf815bc3a31b7fbc238f706e82ef9a48b
iyr <forwardsweep@gmail.com>
2019-09-14 09:59:17 -0600

    tweaked for development

--- heavenLi_pyopengl/plugins/demo/plugin.py
+++ heavenLi_pyopengl/plugins/demo/plugin.py
@@ -3,20 +3,20 @@ from lampClass import *
 class Plugin:
 
     def __init__(self):
+        self.quack = []
+        self.quack.append(Lamp())
+        self.quack[0].setAlias('quack')
+        self.quack[0].setNumBulbs(3)
+        self.quack[0].setArn(0)
+        self.quack[0].setAngle(0)
+        self.quack[0].setID([ord('q'), ord('1')])
         pass
+        return
 
     def update(self):
         pass
 
     def getLamps(self):
-        quack = []
-        #quack.append(Lamp())
-        #quack[0].setAlias('quack')
-        #quack[0].setNumBulbs(3)
-        #quack[0].setArn(0)
-        #quack[0].setAngle(0)
-        #quack[0].setID('q1')
-
-        return quack
+        return self.quack
 
 #if __name__ == '__main__':
