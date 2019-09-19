a7c862c2032dd1f4c83095e5293752b171193a93
iyr <forwardsweep@gmail.com>
2019-09-19 10:58:38 -0600

    initial work on python function ferry freetype character data to opengl/c++

---
+++ heavenLi_pyopengl/textUtils/loadChar.cpp
@@ -0,0 +1,69 @@
+#define GL_GLEXT_PROTOTYPES
+#if defined(_WIN32) || defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__BORLANDC__)
+   #include <windows.h>
+   // These undefs necessary because microsoft
+   #undef near
+   #undef far
+#endif
+#include <GL/gl.h>
+#include <GL/glext.h>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <map>
+#include "characterClass.h"
+
+using namespace std;
+
+PyObject* loadChar_textUtils(PyObject* self, PyObject *args) {
+   PyObject *Pylist;
+   PyObject *Pystring;
+   GLubyte* buffer;
+   GLuint   sizeX, sizeY, bearingX, bearingY, advance;
+
+   // Parse Inputs
+   if ( !PyArg_ParseTuple(args,
+            "OiiiiiO",
+            &Pystring,
+            &sizeX, &sizeY,
+            &bearingX, &bearingY,
+            &advance,
+            &Pylist ) )
+   {
+      Py_RETURN_NONE;
+   }
+
+   // Determine which character we're building
+   const char* inputChars = PyUnicode_AsUTF8(Pystring);
+   GLchar character = inputChars[0];
+
+   // Get length of texture buffer
+   GLuint bufferLength;
+   bufferLength = PyList_Size(Pylist);
+   
+   // Allocate temporary buffer
+   buffer = new GLubyte[bufferLength];
+
+   // Copy contents of Pylist to temporary buffer
+   PyObject* Pylong;
+   for (unsigned int i = 0; i < bufferLength; i++){
+      Pylong      = PyList_GetItem(Pylist, i);
+      buffer[i]   = byte(PyLong_AsLong(Pylong));
+   }
+
+   // Create Character Object
+   Character ch;
+
+   // Assign object parameters
+   ch.setSizeX(sizeX);
+   ch.setSizeY(sizeY);
+   ch.setBearingX(bearingX);
+   ch.setBearingY(bearingY);
+   ch.setAdvance(advance);
+   ch.setBitmap(buffer, bufferLength);
+   
+   Characters.insert(std::pair<GLchar, Character>(character, ch));
+   // Cleanup
+   delete [] buffer;
+   Py_RETURN_NONE;
+}
