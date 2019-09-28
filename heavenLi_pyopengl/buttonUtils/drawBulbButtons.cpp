53e78315289d060af62b83d50539698acff011c6
iyr <forwardsweep@gmail.com>
2019-09-28 14:52:33 -0600

    initial work porting to drawCall class

--- heavenLi_pyopengl/buttonUtils/drawBulbButtons.cpp
+++ heavenLi_pyopengl/buttonUtils/drawBulbButtons.cpp
@@ -1,23 +1,16 @@
 using namespace std;
 
-GLfloat     *bulbButtonCoordBuffer  = NULL; // Stores (X, Y) (float) for each vertex
-GLfloat     *bulbButtonColorBuffer  = NULL; // Stores (R, G, B) (float) for each vertex
-GLushort    *bulbButtonIndices      = NULL; // Stores index corresponding to each vertex
-GLuint      bulbButtonVerts;
-GLuint      vertsPerBulb;
-float*      buttonCoords       = NULL;
-GLint       colorsStart;
-GLint       colorsEnd;
-GLint       detailEnd;
-GLint       prevNumBulbs;
-GLint       prevArn;
-GLfloat     prevAngOffset;
-GLfloat     prevBulbButtonScale;
-GLfloat     prevBulbButtonW2H;
-Matrix      bulbButtonMVP;                  // Transformation matrix passed to shader
-Params      bulbButtonPrevState;            // Stores transformations to avoid redundant recalculation
-GLuint      bulbButtonVBO;                  // Vertex Buffer Object ID
-GLboolean   bulbButtonFirstRun  = GL_TRUE;  // Determines if function is running for the first time (for VBO initialization)
+drawCall bulbButton;
+GLuint   vertsPerBulb;
+GLfloat* buttonCoords       = NULL;
+GLfloat  prevAngOffset;
+GLfloat  prevBulbButtonScale;
+GLfloat  prevBulbButtonW2H;
+GLint    colorsStart;
+GLint    colorsEnd;
+GLint    detailEnd;
+GLint    prevNumBulbs;
+GLint    prevArn;
 
 PyObject* drawBulbButton_drawUtils(PyObject *self, PyObject *args)
 {
@@ -36,7 +29,10 @@ PyObject* drawBulbButton_drawUtils(PyObject *self, PyObject *args)
                gy=0.0f, 
                scale=1.0f, 
                ao=0.0f;
-   GLint arn, numBulbs, circleSegments;
+   GLint    arn, 
+            numBulbs, 
+            circleSegments;
+   GLuint   bulbButtonVerts;
 
    // Parse input arguments
    if (!PyArg_ParseTuple(args, 
@@ -72,11 +68,9 @@ PyObject* drawBulbButton_drawUtils(PyObject *self, PyObject *args)
    }
 
    // Initialize / Update Vertex Geometry and Colors
-   if (  prevNumBulbs            != numBulbs ||
-         bulbButtonCoordBuffer   == NULL     || 
-         bulbButtonColorBuffer   == NULL     || 
-         bulbButtonIndices       == NULL     || 
-         buttonCoords            == NULL     ){
+   if (  prevNumBulbs         != numBulbs ||
+         bulbButton.numVerts  == 0        ||
+         buttonCoords         == NULL     ){
 
       if (numBulbs > 1) {
          printf("Initializing Geometry for Bulb Buttons\n");
@@ -146,104 +140,14 @@ PyObject* drawBulbButton_drawUtils(PyObject *self, PyObject *args)
       // Pack Vertices / Colors into global array buffers
       bulbButtonVerts = verts.size()/2;
 
-      // (Re)allocate vertex buffer
-      if (bulbButtonCoordBuffer == NULL) {
-         bulbButtonCoordBuffer = new GLfloat[bulbButtonVerts*2];
-      } else {
-         delete [] bulbButtonCoordBuffer;
-         bulbButtonCoordBuffer = new GLfloat[bulbButtonVerts*2];
-      }
-
-      // (Re)allocate color buffer
-      if (bulbButtonColorBuffer == NULL) {
-         bulbButtonColorBuffer = new GLfloat[bulbButtonVerts*4];
-      } else {
-         delete [] bulbButtonColorBuffer;
-         bulbButtonColorBuffer = new GLfloat[bulbButtonVerts*4];
-      }
-
-      // (Re)allocate index array
-      if (bulbButtonIndices == NULL) {
-         bulbButtonIndices = new GLushort[bulbButtonVerts];
-      } else {
-         delete [] bulbButtonIndices;
-         bulbButtonIndices = new GLushort[bulbButtonVerts];
-      }
-
-      // Pack bulbButtonIndices, vertex and color bufferes
-      for (unsigned int i = 0; i < bulbButtonVerts; i++){
-         bulbButtonCoordBuffer[i*2]   = verts[i*2];
-         bulbButtonCoordBuffer[i*2+1] = verts[i*2+1];
-         bulbButtonIndices[i]         = i;
-         bulbButtonColorBuffer[i*4+0] = colrs[i*4+0];
-         bulbButtonColorBuffer[i*4+1] = colrs[i*4+1];
-         bulbButtonColorBuffer[i*4+2] = colrs[i*4+2];
-         bulbButtonColorBuffer[i*4+3] = colrs[i*4+3];
-      }
-
-      // Calculate initial Transformation matrix
-      Matrix Ortho;
-      Matrix ModelView;
-
-      float left = -1.0f*w2h, right = 1.0f*w2h, bottom = 1.0f, top = 1.0f, near = 1.0f, far = 1.0f;
-      MatrixLoadIdentity( &Ortho );
-      MatrixLoadIdentity( &ModelView );
-      MatrixOrtho( &Ortho, left, right, bottom, top, near, far );
-      MatrixTranslate( &ModelView, 1.0f*gx, 1.0f*gy, 0.0f );
-      MatrixScale( &ModelView, scale/w2h, scale, 1.0f );
-      MatrixRotate( &ModelView, -ao, 0.0f, 0.0f, 1.0f);
-      MatrixMultiply( &bulbButtonMVP, &ModelView, &Ortho );
-
-      bulbButtonPrevState.ao = ao;
-      bulbButtonPrevState.dx = gx;
-      bulbButtonPrevState.dy = gy;
-      bulbButtonPrevState.sx = scale;
-      bulbButtonPrevState.sy = scale;
-      bulbButtonPrevState.w2h = w2h;
-
       // Update Statemachine Variables
-      prevNumBulbs = numBulbs;
-      prevAngOffset = angularOffset;
-      prevBulbButtonW2H = w2h;
-      prevArn = arn;
-      prevBulbButtonScale = buttonScale;
+      prevNumBulbs         = numBulbs;
+      prevAngOffset        = angularOffset;
+      prevBulbButtonW2H    = w2h;
+      prevArn              = arn;
+      prevBulbButtonScale  = buttonScale;
 
-      // Create buffer object if one does not exist, otherwise, delete and make a new one
-      if (bulbButtonFirstRun == GL_TRUE) {
-         bulbButtonFirstRun = GL_FALSE;
-         glGenBuffers(1, &bulbButtonVBO);
-      } else {
-         glDeleteBuffers(1, &bulbButtonVBO);
-         glGenBuffers(1, &bulbButtonVBO);
-      }
-
-      // Set active VBO
-      glBindBuffer(GL_ARRAY_BUFFER, bulbButtonVBO);
-
-      // Allocate space to hold all vertex coordinate and color data
-      glBufferData(GL_ARRAY_BUFFER, 6*sizeof(GLfloat)*bulbButtonVerts, NULL, GL_STATIC_DRAW);
-
-      // Convenience variables
-      GLintptr offset = 0;
-      GLuint vertAttribCoord = glGetAttribLocation(3, "vertCoord");
-      GLuint vertAttribColor = glGetAttribLocation(3, "vertColor");
-
-      // Load Vertex coordinate data into VBO
-      glBufferSubData(GL_ARRAY_BUFFER, offset, sizeof(GLfloat)*2*bulbButtonVerts, bulbButtonCoordBuffer);
-      // Define how the Vertex coordinate data is layed out in the buffer
-      glVertexAttribPointer(vertAttribCoord, 2, GL_FLOAT, GL_FALSE, 2*sizeof(GLfloat), (GLintptr*)offset);
-      // Enable the vertex attribute
-      glEnableVertexAttribArray(vertAttribCoord);
-
-      // Update offset to begin storing data in latter part of the buffer
-      offset += 2*sizeof(GLfloat)*bulbButtonVerts;
-
-      // Load Vertex coordinate data into VBO
-      glBufferSubData(GL_ARRAY_BUFFER, offset, sizeof(GLfloat)*4*bulbButtonVerts, bulbButtonColorBuffer);
-      // Define how the Vertex color data is layed out in the buffer
-      glVertexAttribPointer(vertAttribColor, 4, GL_FLOAT, GL_FALSE, 4*sizeof(GLfloat), (GLintptr*)offset);
-      // Enable the vertex attribute
-      glEnableVertexAttribArray(vertAttribColor);
+      bulbButton.buildCache(bulbButtonVerts, verts, colrs);
    } 
 
    // Recalculate vertex geometry without expensive vertex/array reallocation
@@ -289,36 +193,33 @@ PyObject* drawBulbButton_drawUtils(PyObject *self, PyObject *args)
          buttonCoords[j*2+0] = tmx;
          buttonCoords[j*2+1] = tmy;
 
-         index = updatePrimEllipseGeometry(tmx, tmy, 0.4f*buttonScale, 0.4f*buttonScale, circleSegments, index, bulbButtonCoordBuffer);
-         index = updateBulbGeometry(tmx, tmy+0.035f, 0.15f*buttonScale, circleSegments, index, bulbButtonCoordBuffer);
+         index = updatePrimEllipseGeometry(tmx, tmy, 0.4f*buttonScale, 0.4f*buttonScale, circleSegments, index, bulbButton.coordCache);
+         index = updateBulbGeometry(tmx, tmy+0.035f, 0.15f*buttonScale, circleSegments, index, bulbButton.coordCache);
       }
 
-      // Set active VBO
-      glBindBuffer(GL_ARRAY_BUFFER, bulbButtonVBO);
-      // Convenience variable
-      GLintptr offset = 0;
-      // Load Vertex Coordinate data into VBO
-      glBufferSubData(GL_ARRAY_BUFFER, offset, sizeof(GLfloat)*2*bulbButtonVerts, bulbButtonCoordBuffer);
+      // Update Statemachine Variables
+      prevNumBulbs = numBulbs;
+      prevAngOffset = angularOffset;
+      prevBulbButtonW2H = w2h;
+      prevArn = arn;
+      prevBulbButtonScale = buttonScale;
+
+      bulbButton.updateCoordCache();
    }
+
    // Vertices / Geometry already calculated
    // Check if colors need to be updated
    int index = 0;
    for (int j = 0; j < numBulbs; j++) {
-      index = updatePrimEllipseColor(circleSegments, faceColor, index, bulbButtonColorBuffer);
+      index = updatePrimEllipseColor(circleSegments, faceColor, index, bulbButton.colorCache);
       float tmbc[4];
       tmbc[0] = bulbColors[j*3+0];
       tmbc[1] = bulbColors[j*3+1];
       tmbc[2] = bulbColors[j*3+2];
       tmbc[3] = 1.0;
-      index = updateBulbColor(circleSegments, tmbc, detailColor, index, bulbButtonColorBuffer);
+      index = updateBulbColor(circleSegments, tmbc, detailColor, index, bulbButton.colorCache);
    }
-   // Update Contents of VBO
-   // Set active VBO
-   glBindBuffer(GL_ARRAY_BUFFER, bulbButtonVBO);
-   // Convenience variable
-   GLintptr offset = 2*sizeof(GLfloat)*bulbButtonVerts;
-   // Load Vertex Color data into VBO
-   glBufferSubData(GL_ARRAY_BUFFER, offset, sizeof(GLfloat)*4*bulbButtonVerts, bulbButtonColorBuffer);
+   bulbButton.updateColorCache();
 
    //PyList_ClearFreeList();
    py_list = PyList_New(numBulbs);
@@ -332,50 +233,8 @@ PyObject* drawBulbButton_drawUtils(PyObject *self, PyObject *args)
    // Cleanup
    delete [] bulbColors;
    
-   // Update Transfomation Matrix if any change in parameters
-   if (  bulbButtonPrevState.ao != ao     ||
-         bulbButtonPrevState.dx != gx     ||
-         bulbButtonPrevState.dy != gy     ||
-         bulbButtonPrevState.sx != scale  ||
-         bulbButtonPrevState.sy != scale  ||
-         bulbButtonPrevState.w2h != w2h   ){
-      
-      Matrix Ortho;
-      Matrix ModelView;
-
-      float left = -1.0f*w2h, right = 1.0f*w2h, bottom = 1.0f, top = 1.0f, near = 1.0f, far = 1.0f;
-      MatrixLoadIdentity( &Ortho );
-      MatrixLoadIdentity( &ModelView );
-      MatrixOrtho( &Ortho, left, right, bottom, top, near, far );
-      MatrixTranslate( &ModelView, 1.0f*gx, 1.0f*gy, 0.0f );
-      MatrixScale( &ModelView, scale/w2h, scale, 1.0f );
-      MatrixRotate( &ModelView, -ao, 0.0f, 0.0f, 1.0f);
-      MatrixMultiply( &bulbButtonMVP, &ModelView, &Ortho );
-
-      bulbButtonPrevState.ao = ao;
-      bulbButtonPrevState.dx = gx;
-      bulbButtonPrevState.dy = gy;
-      bulbButtonPrevState.sx = scale;
-      bulbButtonPrevState.sy = scale;
-      bulbButtonPrevState.w2h = w2h;
-   }
-
-   // Pass Transformation Matrix to shader
-   glUniformMatrix4fv( 0, 1, GL_FALSE, &bulbButtonMVP.mat[0][0] );
-
-   // Set active VBO
-   glBindBuffer(GL_ARRAY_BUFFER, bulbButtonVBO);
-
-   // Define how the Vertex coordinate data is layed out in the buffer
-   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(GLfloat), 0);
-   // Define how the Vertex color data is layed out in the buffer
-   glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4*sizeof(GLfloat), (void*)(2*sizeof(GLfloat)*bulbButtonVerts));
-   //glEnableVertexAttribArray(0);
-   //glEnableVertexAttribArray(1);
-   glDrawArrays(GL_TRIANGLE_STRIP, 0, bulbButtonVerts);
-
-   // Unbind Buffer Object
-   glBindBuffer(GL_ARRAY_BUFFER, 0);
+   bulbButton.updateMVP(gx, gy, scale, scale, ao, w2h);
+   bulbButton.draw();
 
    return py_list;
 }
