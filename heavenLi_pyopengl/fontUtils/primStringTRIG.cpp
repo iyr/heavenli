44791648ab6ae8ed812b9ff18e7bef1a788c1781
iyr <forwardsweep@gmail.com>
2019-11-10 01:51:45 -0700

    formatting, minor tweaks, accepts atlas pointer

--- heavenLi_pyopengl/fontUtils/primStringTRIG.cpp
+++ heavenLi_pyopengl/fontUtils/primStringTRIG.cpp
@@ -9,6 +9,7 @@ unsigned int defineString(
       float       x,             // x-postion
       float       y,             // y-postion
       std::string inputString,   // Input string to draw
+      float       alignment,     // 0.0=left, 0.5=center, 1.0=right
       textAtlas*  atlas,         // texture atlas to draw characters from
       float*      textColor,     // Polygon Color                                         
       std::vector<float> &verts, // Input Vector of x,y coordinates                       
@@ -16,9 +17,9 @@ unsigned int defineString(
       std::vector<float> &colrs  // Input Vector of r,g,b values                          
       ) {
 
-   int c = 0;
+   int c = 0, lastLineVert=0;
    character* tmg;
-   float ax;
+   float ax, lineLength=0.0f;
 
    GLuint stringLen = inputString.size();
    const char* inputChars = inputString.c_str();
@@ -26,7 +27,7 @@ unsigned int defineString(
    for (unsigned int i = 0; i < stringLen; i++) {
       c = inputChars[i];
 
-      tmg = &quack->glyphData[c];
+      tmg = &atlas->glyphData[c];
 
       // Only update non-control characters
       if (c >= 32) {
@@ -36,16 +37,49 @@ unsigned int defineString(
 
       // Shift downward and reset x position for line breaks
       if (c == (int)'\n') {
-         y -= (float)quack->faceSize;
+         y -= (float)atlas->faceSize;
          x = 0.0f;
+         lineLength = 0.0f;
       }
 
       defineChar(
             x-ax, y, 
             c,
-            quack,
+            atlas,
             textColor, 
             verts, texuv, colrs);
+   }
+
+   for (unsigned int i = 0; i < stringLen; i++) {
+      c = inputChars[i];
+
+      // Shift downward and reset x position for line breaks
+      if (  c == (int)'\n'    || 
+            i == stringLen-1  ){
+
+         if (i != stringLen-1){
+            y -= (float)atlas->faceSize;
+            x = 0.0f;
+         }
+
+         // Get the length of the line (furthest quad vert on left to furthest on right)
+         for (unsigned int j = lastLineVert; j < i*6; j++)
+            if(verts[j*2] > lineLength)
+               lineLength = verts[j*2];
+
+         // Shift the line left by alignment coeefficient
+         // 0.0 = left-aligned
+         // 0.5 = center-aligned
+         // 1.0 = right-aligned
+         for (unsigned int j = lastLineVert; j < (i+1)*6; j++)
+            verts[j*2] -= alignment*lineLength;
+
+         // Reset linelength
+         lineLength = 0.0f;
+
+         // set the index of the last line
+         lastLineVert = i*6;
+      }
    }
 
    return verts.size()/2;
@@ -55,45 +89,64 @@ unsigned int updateString(
       float       x,             // x-postion
       float       y,             // y-postion
       std::string inputString,   // Input string to draw
+      float       alignment,     // 0.0=left, 0.5=center, 1.0=right
       textAtlas*  atlas,         // texture atlas to draw characters from
       GLuint      index,         // Index of where to start writing to input arrays
       float*      verts,         // Input Vector of x,y coordinates                       
       float*      texuv          // Input Vector of u,y texture coordinates
       ){
 
-   int c = 0;
-   float ax;
+   int c = 0, lastLineVert=0;
+   float ax, lineLength=0.0f;
    GLuint stringLen = inputString.size();
    const char* inputChars = inputString.c_str();
    character* tmg;
 
    for (unsigned int i = 0; i < stringLen; i++) {
+      c = inputChars[i];
+      tmg = &atlas->glyphData[c];
 
-      //if (i < stringLen){
-         c = inputChars[i];
+      // Only update non-control characters
+      if (c >= 32 && c != (int)'\n') {
+         ax =  (float)tmg->advanceX*0.015625f; // divide by 64
+         x +=  ax;
+      }
 
-         tmg = &quack->glyphData[c];
+      index = updateChar(
+            x-ax, y, 
+            c,
+            atlas,
+            index,
+            verts, 
+            texuv);
 
-         // Only update non-control characters
-         if (c >= 32) {
-            ax =  (float)tmg->advanceX*0.015625f; // divide by 64
-            x +=  ax;
-         }
+      // Shift downward and reset x position for line breaks
+      if (  c == (int)'\n'    || 
+            i == stringLen-1  ){
 
-         // Shift downward and reset x position for line breaks
-         if (c == (int)'\n') {
-            y -= (float)quack->faceSize;
+         if (i != stringLen-1){
+            y -= (float)atlas->faceSize;
             x = 0.0f;
          }
 
-         index = updateChar(
-               x-ax, y, 
-               c,
-               quack,
-               index,
-               verts, 
-               texuv);
-      //}
+         // Get the length of the line (furthest quad vert on left to furthest on right)
+         for (unsigned int j = lastLineVert; j < (i+1)*6; j++)
+            if(verts[j*2] > lineLength)
+               lineLength = verts[j*2];
+
+         // Shift the line left by alignment coeefficient
+         // 0.0 = left-aligned
+         // 0.5 = center-aligned
+         // 1.0 = right-aligned
+         for (unsigned int j = lastLineVert; j < (i+1)*6; j++)
+            verts[j*2] -= alignment*lineLength;
+
+         // Reset linelength
+         lineLength = 0.0f;
+
+         // set the index of the last line
+         lastLineVert = i*6;
+      }
    }
 
    return index;
