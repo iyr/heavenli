4a2948fbc63608d2772e53f61249b90d5aa79fce
iyr <forwardsweep@gmail.com>
2019-11-09 19:59:59 -0700

    added header for forward declaration

---
+++ heavenLi_pyopengl/fontUtils/drawText.h
@@ -0,0 +1,13 @@
+// C/C++ function for drawing text
+// forward declaration
+void drawText(
+      std::string inputString,   // string of text draw
+      GLfloat     gx,            // X position
+      GLfloat     gy,            // Y position
+      GLfloat     sx,            // X scale
+      GLfloat     sy,            // Y scale
+      GLfloat     w2h,           // width to height ration
+      GLfloat*    textColor,     // color of text
+      GLfloat*    faceColor      // color of backdrop
+      );
+
