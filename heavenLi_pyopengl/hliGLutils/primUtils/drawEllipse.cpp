a1b7cb62c234e4ffc622bd9eb92159a6df4368cd
iyr <forwardsweep@gmail.com>
2019-12-07 06:13:42 -0700

    initial commit

---
+++ heavenLi_pyopengl/hliGLutils/primUtils/drawEllipse.cpp
@@ -0,0 +1,104 @@
+using namespace std;
+
+extern std::map<std::string, drawCall> drawCalls;
+
+void drawEllipse(
+      GLfloat     gx, 
+      GLfloat     gy,
+      GLfloat     sx,
+      GLfloat     sy,
+      GLfloat     w2h,
+      GLfloat*    faceColor,
+      drawCall*   ellipseButton
+      );
+
+PyObject* drawEllipse_hliGLutils(PyObject* self, PyObject *args) {
+   PyObject *faceColorPyTup;
+   float gx, gy, sx, sy, w2h;
+   float faceColor[4];
+
+   // Parse Inputs
+   if ( !PyArg_ParseTuple(args,
+            "fffffO",
+            &gx, &gy,
+            &sx, &sy,
+            &w2h,
+            &faceColorPyTup
+            ) )
+   {
+      Py_RETURN_NONE;
+   }
+
+   faceColor[0] = float(PyFloat_AsDouble(PyTuple_GetItem(faceColorPyTup, 0)));
+   faceColor[1] = float(PyFloat_AsDouble(PyTuple_GetItem(faceColorPyTup, 1)));
+   faceColor[2] = float(PyFloat_AsDouble(PyTuple_GetItem(faceColorPyTup, 2)));
+   faceColor[3] = float(PyFloat_AsDouble(PyTuple_GetItem(faceColorPyTup, 3)));
+
+   if (drawCalls.count("ellipseButton") <= 0)
+      drawCalls.insert(std::make_pair("ellipseButton", drawCall()));
+   drawCall* ellipseButton = &drawCalls["ellipseButton"];
+
+   drawEllipse(
+         gx, 
+         gy,
+         sx,
+         sy,
+         w2h,
+         faceColor,
+         ellipseButton
+         );
+
+   Py_RETURN_NONE;
+}
+
+void drawEllipse(
+      GLfloat     gx, 
+      GLfloat     gy,
+      GLfloat     sx,
+      GLfloat     sy,
+      GLfloat     w2h,
+      GLfloat*    faceColor,
+      drawCall*   ellipseButton
+      ){
+
+   GLuint ellipseVerts;
+   ellipseButton->setNumColors(1);
+   ellipseButton->setColorQuartet(0, faceColor);
+
+   int circleSegments = 60;
+   if (ellipseButton->numVerts == 0){
+
+      printf("Initializing Geometry for Ellipse Button\n");
+      vector<GLfloat> verts;
+      vector<GLfloat> colrs;
+
+      // Draw button face
+      defineEllipse(
+            0.0f, 0.0f,
+            1.0f, 1.0f,
+            circleSegments,
+            faceColor,
+            verts, colrs);
+
+      ellipseVerts = verts.size()/2;
+
+      ellipseButton->buildCache(ellipseVerts, verts, colrs);
+   }
+
+   if (ellipseButton->colorsChanged) {
+      unsigned int index = 0;
+      // Draw button face
+      index = updateEllipseColor(
+            circleSegments,
+            faceColor,
+            index, 
+            ellipseButton->colorCache);
+
+      ellipseButton->updateColorCache();
+   }
+
+   ellipseButton->updateMVP(gx, gy, sx, sy, 0.0f, w2h);
+   ellipseButton->draw();
+
+   return;
+}
---
+++ heavenLi_pyopengl/hliGLutils/primUtils/drawPill.cpp
@@ -0,0 +1,168 @@
+using namespace std;
+
+extern std::map<std::string, drawCall> drawCalls;
+
+void drawPill(
+      GLfloat     px, 
+      GLfloat     py,
+      GLfloat     qx,
+      GLfloat     qy,
+      GLfloat     radius,
+      GLfloat     w2h,
+      GLfloat*    pColor,
+      GLfloat*    qColor,
+      drawCall*   pillButton
+      );
+
+PyObject* drawPill_hliGLutils(PyObject* self, PyObject *args) {
+   PyObject *pColorPyTup;
+   PyObject *qColorPyTup;
+   float px, py, qx, qy, w2h, radius;
+   float pColor[4];
+   float qColor[4];
+
+   // Parse Inputs
+   if ( !PyArg_ParseTuple(args,
+            "ffffffOO",
+            &px, &py,
+            &qx, &qy,
+            &radius,
+            &w2h,
+            &pColorPyTup,
+            &qColorPyTup
+            ) )
+   {
+      Py_RETURN_NONE;
+   }
+
+   pColor[0] = float(PyFloat_AsDouble(PyTuple_GetItem(pColorPyTup, 0)));
+   pColor[1] = float(PyFloat_AsDouble(PyTuple_GetItem(pColorPyTup, 1)));
+   pColor[2] = float(PyFloat_AsDouble(PyTuple_GetItem(pColorPyTup, 2)));
+   pColor[3] = float(PyFloat_AsDouble(PyTuple_GetItem(pColorPyTup, 3)));
+
+   qColor[0] = float(PyFloat_AsDouble(PyTuple_GetItem(qColorPyTup, 0)));
+   qColor[1] = float(PyFloat_AsDouble(PyTuple_GetItem(qColorPyTup, 1)));
+   qColor[2] = float(PyFloat_AsDouble(PyTuple_GetItem(qColorPyTup, 2)));
+   qColor[3] = float(PyFloat_AsDouble(PyTuple_GetItem(qColorPyTup, 3)));
+
+   if (drawCalls.count("pillButton") <= 0)
+      drawCalls.insert(std::make_pair("pillButton", drawCall()));
+   drawCall* pillButton = &drawCalls["pillButton"];
+
+   drawPill(
+         px, 
+         py,
+         qx,
+         qy,
+         radius,
+         w2h,
+         pColor,
+         qColor,
+         pillButton
+         );
+
+   Py_RETURN_NONE;
+}
+
+GLfloat prevPx;
+GLfloat prevPy;
+GLfloat prevQx;
+GLfloat prevQy;
+GLfloat prevRad;
+
+void drawPill(
+      GLfloat     px, 
+      GLfloat     py,
+      GLfloat     qx,
+      GLfloat     qy,
+      GLfloat     radius,
+      GLfloat     w2h,
+      GLfloat*    pColor,
+      GLfloat*    qColor,
+      drawCall*   pillButton
+      ){
+
+   GLuint pillVerts;
+   pillButton->setNumColors(2);
+   pillButton->setColorQuartet(0, pColor);
+   pillButton->setColorQuartet(1, qColor);
+
+   int circleSegments = 60;
+   if (pillButton->numVerts == 0){
+
+      printf("Initializing Geometry for Pill Button\n");
+      vector<GLfloat> verts;
+      vector<GLfloat> colrs;
+
+      // Draw button face
+      definePill(
+            px, py,
+            qx, qy,
+            radius,
+            circleSegments,
+            pColor,
+            qColor,
+            verts, 
+            colrs
+            );
+
+      pillVerts = verts.size()/2;
+
+      prevPx   = px;
+      prevPy   = py;
+      prevQx   = qx;
+      prevQy   = qy;
+      prevRad  = radius;
+
+      pillButton->buildCache(pillVerts, verts, colrs);
+   }
+
+   if (  prevPx   != px    ||
+         prevPy   != py    ||
+         prevQx   != qx    ||
+         prevQy   != qy    ||
+         prevRad  != radius){
+
+      GLint index = 0;
+
+      index = updatePillGeometry(
+            px, py,
+            qx, qy,
+            radius,
+            circleSegments,
+            index,
+            pillButton->coordCache
+            );
+
+      prevPx   = px;
+      prevPy   = py;
+      prevQx   = qx;
+      prevQy   = qy;
+      prevRad  = radius;
+
+      pillButton->updateCoordCache();
+   }
+
+   if (pillButton->colorsChanged) {
+      unsigned int index = 0;
+      // Draw button face
+      index = updatePillColor(
+            circleSegments,
+            pColor,
+            qColor,
+            index, 
+            pillButton->colorCache);
+
+      pillButton->updateColorCache();
+   }
+
+   float gx=0.0f, 
+         gy=0.0f, 
+         sx=1.0f, 
+         sy=1.0f;
+
+   pillButton->updateMVP(gx, gy, sx, sy, 0.0f, w2h);
+   pillButton->draw();
+
+   return;
+}
