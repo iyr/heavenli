8cafb727a506a8129802adddef4924fbdca7e9b4
iyr <forwardsweep@gmail.com>
2020-08-16 21:26:38 -0600

    deprecated unused variable

--- heavenLi_pyopengl/hliGLutils/arnUtils.h
+++ heavenLi_pyopengl/hliGLutils/arnUtils.h
@@ -1,7 +1,7 @@
 /*
  * Dynamic iconography draw code for different arrangements
  */
-float offScreen = 100.0;            // Used for moving geometry offscreen, will likely get depricated
+//float offScreen = 100.0;            // Used for moving geometry offscreen, will likely get depricated
 #include "arnUtils/arnCircle.cpp"   // Utilities for drawing icon+background for circular arrangements
 #include "arnUtils/arnLinear.cpp"   // Utilities for drawing icon+background for linear arrangements
 #include "arnUtils/drawIcon.cpp"    // Utility for drawing lamp icons
