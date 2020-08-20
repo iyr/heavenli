40b15830ca772d2329578746a7c46c095bd0715c
iyr <forwardsweep@gmail.com>
2020-08-20 08:23:16 -0600

    deprecated obsolete globals, added shaderprograms global

--- heavenLi_pyopengl/hliGLutils/initUtils.h
+++ heavenLi_pyopengl/hliGLutils/initUtils.h
@@ -1,10 +1,12 @@
-//#include "initUtils/initHLIobjects.cpp"   // initializes hli related global objects
 
-GLuint buildShader(const GLchar* vertexSourceFile, const GLchar* fragmentSourceFile);
-std::vector<std::string> drawQue;
-std::map<std::string, drawCall>     drawCalls;
-std::map<std::string, textAtlas>    textFonts;
-std::map<std::string, GLuint>       shaders;
-std::string selectedAtlas;
+/*
+ * Initialize globals
+ */
+vector<string> drawQue;
+map<string, drawCall>     drawCalls;     // Contains all draw call objects
+map<string, textAtlas>    textFonts;     // Contains all text atlases
+//map<string, GLuint>       shaders;       // Contains all shader ids
+map<string, shaderProg>   shaderPrograms;// Contains shader programs
+string selectedAtlas;
 GLuint   whiteTex;
 #include "initUtils/initShaders.cpp"      // Code that builds a shaderProgram (vert+frag) from source
