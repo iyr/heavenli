46dc51c4d22d8f6886b70fb34d7f8bc8a54dc96b
iyr <forwardsweep@gmail.com>
2020-09-19 16:41:02 -0600

    defined basic struct for storing default shader vertex attribute locations (string variable names)

--- heavenLi_pyopengl/hliGLutils/vertAttribStruct.h
+++ heavenLi_pyopengl/hliGLutils/vertAttribStruct.h
@@ -1,19 +1,76 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
+#ifndef vertAttribMetadataClass
+#define vertAttribMetadataClass
 
-typedef struct
-{
-   std::string locationString;   // Name of the attribute in the shader
+class vertAttribMetadata {
+   public:
+      string locationString = "UNNAMED VERTEX ATTRIBUTE";   // Name of the attribute in the shader
 
-   GLubyte  vectorSize  = 0,     // Number of vector components (1-4)
-            vectorOffset= 0;     // 
-   GLuint   attribIndex = 0;     // May get deprecated
+      // (De)constructors
+      vertAttribMetadata(void);
+      virtual ~vertAttribMetadata(void);
 
-   //GLfloat* clientCache = NULL;
+      // Setters
+      void setLocationString  (string newLocString);
+      void setVectorSize      (GLubyte newVecSize);
+      void setVectorOffset    (GLubyte newOffset);
+      void setAttribIndex     (GLuint  newIndex);
 
-} vertAttribMetadata;
+      // Getters
+      string   getLocationString (void);
+      GLubyte  getVectorSize     (void);
+      GLubyte  getVectorOffset   (void);
+      GLuint   getAttribIndex    (void);
+     
+   //private:
+   protected:
+      GLubyte  vectorSize  = 0,  // Number of vector components (1-4)
+               vectorOffset= 0;  // May get deprecated
+      GLuint   attribIndex = 0;  // May get deprecated
+};
 
-#ifdef __cplusplus
-}
+vertAttribMetadata::vertAttribMetadata(void){
+   return;
+};
+
+vertAttribMetadata::~vertAttribMetadata(void){
+   return;
+};
+
+void vertAttribMetadata::setLocationString(string newLocString){
+   this->locationString = newLocString;
+   return;
+};
+
+void vertAttribMetadata::setVectorSize(GLubyte newVecSize){
+   //printf("new vector size: %d\n", (GLuint)newVecSize);
+   this->vectorSize = newVecSize;
+   return;
+};
+
+void vertAttribMetadata::setVectorOffset(GLubyte newOffset){
+   this->vectorOffset = newOffset;
+   return;
+};
+
+void vertAttribMetadata::setAttribIndex(GLuint newIndex){
+   this->attribIndex = newIndex;
+   return;
+};
+
+string vertAttribMetadata::getLocationString(void){
+   return this->locationString;
+};
+
+GLubyte vertAttribMetadata::getVectorSize(void){
+   return this->vectorSize;
+};
+
+GLubyte vertAttribMetadata::getVectorOffset(void){
+   return this->vectorOffset;
+};
+
+GLuint vertAttribMetadata::getAttribIndex(void){
+   return this->attribIndex;
+};
+
 #endif
