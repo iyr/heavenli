0c04dbcef8fbdfc3fb82a40e122345d23138bc74
iyr <forwardsweep@gmail.com>
2020-10-30 01:25:35 -0600

    fixed geometry to support face culling

--- heavenLi_pyopengl/hliGLutils/primUtils/primBulbDEGEN.cpp
+++ heavenLi_pyopengl/hliGLutils/primUtils/primBulbDEGEN.cpp
@@ -8,14 +8,14 @@ using namespace std;
 
 // Append vertices to input vectors
 int defineBulb(
-      float bx,                  /* X-Coordinate                                          */
-      float by,                  /* Y-Coordinate                                          */
-      float bs,                  /* Scale 2.0=spans display before GL scaling             */
-      char  circleSegments,      /* Number of sides                                       */
-      float *color,              /* Polygon Color                                         */
-      float *detailColor,        /* Accent Color                                          */
-      std::vector<float> &verts, /* Input Vector of x,y coordinates                       */
-      std::vector<float> &colrs  /* Input Vector of r,g,b values                          */
+      float bx,                  // X-Coordinate
+      float by,                  // Y-Coordinate
+      float bs,                  // Scale 2.0=spans display before GL scaling
+      char  circleSegments,      // Number of sides
+      float *color,              // Polygon Color
+      float *detailColor,        // Accent Color
+      std::vector<float> &verts, // Input Vector of x,y coordinates
+      std::vector<float> &colrs  // Input Vector of r,g,b values
       ){
    float thickness = 0.3f;
    circleSegments /= 2;
@@ -25,9 +25,9 @@ int defineBulb(
    defineQuad2pt(bx-bs*thickness, by-bs*2.25f, bx+bs*thickness, by-bs*2.25f-bs*thickness, detailColor, verts, colrs);
    defineArch(bx+bs*thickness, by-bs*2.25f, 0.0f, 0.0f, 270.0f, 0.0f, bs*thickness, circleSegments, detailColor, verts, colrs);
    defineArch(bx-bs*thickness, by-bs*2.25f, 0.0f, 0.0f, 180.0f, 270.0f, bs*thickness, circleSegments, detailColor, verts, colrs);
-   defineArch(bx+(bs+bs*thickness*0.5f)*float(cos(degToRad(310.0f))), by+(bs+bs*thickness*0.5f)*float(sin(degToRad(310.0f))), 0.0f, 0.0f, 125.0f, 310.0f, bs*thickness*0.5f, circleSegments, detailColor, verts, colrs);
+   defineArch(bx+(bs+bs*thickness*0.5f)*cosf(degToRad(310.0f)), by+(bs+bs*thickness*0.5f)*sinf(degToRad(310.0f)), 0.0f, 0.0f, 125.0f, 310.0f, bs*thickness*0.5f, circleSegments, detailColor, verts, colrs);
    defineQuad2pt(bx-bs*thickness, by-bs*2.25f, bx-bs*thickness*2.0f, by-bs*1.6f, detailColor, verts, colrs);
-   defineQuad2pt(bx+bs*thickness, by-bs*2.25f, bx+bs*thickness*2.0f, by-bs*1.6f, detailColor, verts, colrs);
+   defineQuad2pt(bx+bs*thickness, by-bs*1.6f, bx+bs*thickness*2.0f, by-bs*2.25f, detailColor, verts, colrs);
    defineArch(bx, by-bs*1.6f, bs*thickness, bs*thickness, 0.0f, 90.0f, bs*thickness, circleSegments, detailColor, verts, colrs);
    defineArch(bx-bs*thickness*1.5f, by-bs*1.6f, 0.0f, 0.0f, 0.0f, 180.0f, bs*thickness*0.5f, circleSegments, detailColor, verts, colrs);
 
@@ -35,12 +35,12 @@ int defineBulb(
 }
 
 int updateBulbGeometry(
-      float bx,               /* X-Coordinate                                    */
-      float by,               /* Y-Coordinate                                    */
-      float bs,               /* Scale 2.0=spans display before GL scaling       */
-      char  circleSegments,   /* Number of sides                                 */
-      int   index,            /* Index of where to start writing to input arrays */
-      float *verts            /* Input Array of x,y coordinates                  */
+      float bx,               // X-Coordinate
+      float by,               // Y-Coordinate
+      float bs,               // Scale 2.0=spans display before GL scaling
+      char  circleSegments,   // Number of sides
+      int   index,            // Index of where to start writing to input arrays
+      float *verts            // Input Array of x,y coordinates
       ){
    float thickness = 0.3f;
    circleSegments /= 2;
@@ -50,20 +50,20 @@ int updateBulbGeometry(
    index = updateQuad2ptGeometry(bx-bs*thickness, by-bs*2.25f, bx+bs*thickness, by-bs*2.25f-bs*thickness, index, verts);
    index = updateArchGeometry(bx+bs*thickness, by-bs*2.25f, 0.0f, 0.0f, 270.0f, 0.0f, bs*thickness, circleSegments, index, verts);
    index = updateArchGeometry(bx-bs*thickness, by-bs*2.25f, 0.0f, 0.0f, 180.0f, 270.0f, bs*thickness, circleSegments, index, verts);
-   index = updateArchGeometry(bx+(bs+bs*thickness*0.5f)*float(cos(degToRad(310.0f))), by+(bs+bs*thickness*0.5f)*float(sin(degToRad(310.0f))), 0.0f, 0.0f, 125.0f, 310.0f, bs*thickness*0.5f, circleSegments, index, verts);
+   index = updateArchGeometry(bx+(bs+bs*thickness*0.5f)*cosf(degToRad(310.0f)), by+(bs+bs*thickness*0.5f)*sinf(degToRad(310.0f)), 0.0f, 0.0f, 125.0f, 310.0f, bs*thickness*0.5f, circleSegments, index, verts);
    index = updateQuad2ptGeometry(bx-bs*thickness, by-bs*2.25f, bx-bs*thickness*2.0f, by-bs*1.6f, index, verts);
-   index = updateQuad2ptGeometry(bx+bs*thickness, by-bs*2.25f, bx+bs*thickness*2.0f, by-bs*1.6f, index, verts);
+   index = updateQuad2ptGeometry(bx+bs*thickness, by-bs*1.6f, bx+bs*thickness*2.0f, by-bs*2.25f, index, verts);
    index = updateArchGeometry(bx, by-bs*1.6f, bs*thickness, bs*thickness, 0.0f, 90.0f, bs*thickness, circleSegments, index, verts);
    index = updateArchGeometry(bx-bs*thickness*1.5f, by-bs*1.6f, 0.0f, 0.0f, 0.0f, 180.0f, bs*thickness*0.5f, circleSegments, index, verts);
    return index;
 }
 
 int updateBulbColor(
-      char  circleSegments,   /* Number of sides                                 */
-      float *color,           /* Polygon Color                                   */
-      float *detailColor,     /* Accent Color                                    */
-      int   index,            /* Index of where to start writing to input arrays */
-      float *colrs            /* Input Vector of r,g,b values                    */
+      char  circleSegments,   // Number of sides
+      float *color,           // Polygon Color
+      float *detailColor,     // Accent Color
+      int   index,            // Index of where to start writing to input arrays
+      float *colrs            // Input Vector of r,g,b values
       ){
    circleSegments /= 2;
    index = updateEllipseColor(circleSegments, color, index, colrs);
